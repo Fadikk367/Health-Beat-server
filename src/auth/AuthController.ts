af5abb476976ca94861685a9d56a9bc644f9b2df
Adrian Furman <fadikk367@gmail.com>
2021-01-20 00:04:25 +0100

    Fix signUp handler

--- src/auth/AuthController.ts
+++ src/auth/AuthController.ts
@@ -39,20 +39,23 @@ export default class AuthController extends Controller {
 
         res.status(200).json({ token, user });
       } else {
-        res.status(400).json({ message: 'Invalid user credentials' });
+        res.status(404).json({ message: 'Invalid user credentials' });
       }
     } else {
-      res.status(400).json({ message: 'Invalid user credentials' });
+      res.status(404).json({ message: 'Invalid user credentials' });
     }
   }
 
   private signUp: RequestHandler = async (req, res): Promise<void> => {
     const registerUserDto: RegisterUserDto = req.body;
 
-    const result = await this.userRepository.registerUser(registerUserDto);
+    const user = await this.userRepository.registerUser(registerUserDto);
+    const TOKEN_SECRET = process.env.TOKEN_SECRET as string;
+    const token = await jwt.sign({ _id: user._id, firstName: user.firstName, lastName: user.lastName }, TOKEN_SECRET);
 
     res.status(201).send({
-      user: result
+      user,
+      token,
     })
   }
 }
